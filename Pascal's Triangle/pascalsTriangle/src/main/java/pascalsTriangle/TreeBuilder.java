bdfba188e59e20228b8bc38a29828e87a5320c07
Conor Thomason <conor.thomason@gmail.com>
2019-03-12 22:27:30 -0400

    Revert "Plan to revert"
    This reverts commit 745ba46cf9b7936b49a3659845af65e6a503652c.

--- Pascal's Triangle/pascalsTriangle/src/main/java/pascalsTriangle/TreeBuilder.java
+++ Pascal's Triangle/pascalsTriangle/src/main/java/pascalsTriangle/TreeBuilder.java
@@ -1,28 +1,46 @@
 package pascalsTriangle;
 
-import com.fxgraph.cells.RectangleCell;
-import com.fxgraph.graph.Graph;
-import com.fxgraph.graph.ICell;
-import com.fxgraph.graph.Model;
-import com.fxgraph.*;
+import graph.VisEdge;
+import graph.VisGraph;
+import graph.VisNode;
 
 public class TreeBuilder {
 	private PascalTree tree;
-	private Graph graph = new Graph();
-	private Model model = graph.getModel();
+	private VisGraph graph;
 	private PascalNode currentNode;
 
 	public TreeBuilder(int initialSize) {
 		this.tree = new PascalTree(initialSize);
+		graph = new VisGraph();
 		buildGraph();
 	}
 	public void buildGraph() {
-		graph.beginUpdate();
-		final ICell cell = new RectangleCell();
-		model.addCell(cell);
-		graph.endUpdate();
+		currentNode = tree.getRootNode();
+		PascalNode initialNode = currentNode;
+		int nodeId = 1;
+		VisNode node = new VisNode(1, currentNode.getValue().toString());
+		VisNode leftNode;
+		VisNode rightNode;
+		VisEdge leftEdge;
+		VisEdge rightEdge;
+		graph.addNodes(node);
+		do {
+			do  {
+				leftNode = new VisNode(nodeId++, currentNode.getNextLeft().getValue().toString());
+				rightNode = new VisNode(nodeId++, currentNode.getNextRight().getValue().toString());
+				leftEdge = new VisEdge(node, leftNode, "", "");
+				rightEdge = new VisEdge(node, rightNode, "", "");
+				graph.addNodes(leftNode, rightNode);
+				graph.addEdges(leftEdge, rightEdge);
+				if (currentNode.hasPreviousRight()) {
+					currentNode = currentNode.getPreviousRight();
+					currentNode = currentNode.getNextRight();
+				}
+			} while (currentNode.hasPreviousRight());
+			initialNode = initialNode.getNextLeft();
+		} while (initialNode.hasNextLeft());
 	}
-	public Graph getGraph() {
+	public VisGraph getGraph() {
 		return graph;
 	}
 }
